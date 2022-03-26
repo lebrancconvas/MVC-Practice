b6c4741a95893717131c6a6e3df98b9accaa19a6
lebrancconvas <lebrancconvas@gmail.com>
2022-03-27 03:31:33 +0700

    🔨 Add new route.

--- MVC01/src/controllers/router.ts
+++ MVC01/src/controllers/router.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import BusinessLogic from './logic';
 
 const routes = express();
 
 routes.get('/', (_req: express.Request, res: express.Response) => {
 	res.render('./index.ejs');
+})
+
+routes.get('/data', (_req: express.Request, res: express.Response) => {
+	const logic = new BusinessLogic();
+	const data = logic.arrayManagement();
+	res.status(200).json(data);
 })
 
 export {routes};
